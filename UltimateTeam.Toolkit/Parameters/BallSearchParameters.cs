f7100e63c0b39a2279b96d326a2cfac36d76fecf
BogdiG <bogdangoo@Bogdans-MacBook-Pro.local>
2013-09-25 16:18:42 +0100

    Added Ball Search

---
+++ UltimateTeam.Toolkit/Parameters/BallSearchParameters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace UltimateTeam.Toolkit.Parameters 
+{
+    public class BallSearchParameters : SearchParameters
+    {
+        public BallSearchParameters()
+            : base(ResourceType.Ball)
+        {
+        }
+
+        internal override string BuildUriString(ref string uriString)
+        {
+      
+            uriString += "&type=" + Type.ToLower();
+
+            if (MinBuy > 0)
+                uriString += "&minb=" + MinBuy;
+
+            if (MaxBuy > 0)
+                uriString += "&maxb=" + MaxBuy;
+
+            if (MinBid > 0)
+                uriString += "&micr=" + MinBid;
+
+            if (MaxBid > 0)
+                uriString += "&macr=" + MaxBid;
+
+
+            return uriString;
+        }
+    }
+}
+
+     
