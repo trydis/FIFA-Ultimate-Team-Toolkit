e8c1d9a299b6fd29b7aec04b40acdf77be96ba25
Morten Trydal <trydis@hotmail.com>
2015-09-20 21:58:13 +0200

    Updated resources

--- UltimateTeam.Toolkit/Constants/Resources.cs
+++ UltimateTeam.Toolkit/Constants/Resources.cs
@@ -2,33 +2,33 @@
 {
     internal class Resources
     {
-        public const string Validate = "https://www.easports.com/iframe/fut15/p/ut/game/fifa15/phishing/validate";
+        public const string Validate = "https://www.easports.com/iframe/fut16/p/ut/game/fifa16/phishing/validate";
 
-        public const string Auth = "https://www.easports.com/iframe/fut15/p/ut/auth";
+        public const string Auth = "https://www.easports.com/iframe/fut16/p/ut/auth";
 
         public const string LoggedIn = "https://www.easports.com/fifa/api/isUserLoggedIn";
 
         public const string Home = "https://www.easports.com/fifa/ultimate-team/web-app";
 
-        public const string NucleusId = "https://www.easports.com/iframe/fut15/?locale=en_US&baseShowoffUrl=https%3A%2F%2Fwww.easports.com%2Ffifa%2Fultimate-team%2Fweb-app%2Fshow-off&guest_app_uri=http%3A%2F%2Fwww.easports.com%2Ffifa%2Fultimate-team%2Fweb-app";
+        public const string NucleusId = "https://www.easports.com/iframe/fut16/?locale=en_US&baseShowoffUrl=https%3A%2F%2Fwww.easports.com%2Ffifa%2Fultimate-team%2Fweb-app%2Fshow-off&guest_app_uri=http%3A%2F%2Fwww.easports.com%2Ffifa%2Fultimate-team%2Fweb-app";
 
-        public const string Shards = "https://www.easports.com/iframe/fut15/p/ut/shards?_={0}";
+        public const string Shards = "https://www.easports.com/iframe/fut16/p/ut/shards/v2?_={0}";
 
-        public const string AccountInfo = " https://www.easports.com/iframe/fut15/p/ut/game/fifa15/user/accountinfo?sku=FUT15WEB&_={0}";
+        public const string AccountInfo = " https://www.easports.com/iframe/fut16/p/ut/game/fifa16/user/accountinfo?sku=FUT16WEB&_={0}";
 
-        public const string BaseShowoff = "http://www.easports.com/iframe/fut/?baseShowoffUrl=http%3A%2F%2Fwww.easports.com%2Fuk%2Ffifa%2Ffootball-club%2Fultimate-team%2Fshow-off&guest_app_uri=http%3A%2F%2Fwww.easports.com%2Fuk%2Ffifa%2Ffootball-club%2Fultimate-team&locale=en_GB";
+        public const string BaseShowoff = "http://www.easports.com/iframe/fut16/?baseShowoffUrl=https%3A%2F%2Fwww.easports.com%2Fuk%2Ffifa%2Fultimate-team%2Fweb-app%2Fshow-off&guest_app_uri=http%3A%2F%2Fwww.easports.com%2Fuk%2Ffifa%2Fultimate-team%2Fweb-app&locale=en_GB";
 
-        public const string Item = "https://fifa15.content.easports.com/fifa/fltOnlineAssets/8D941B48-51BB-4B87-960A-06A61A62EBC0/2015/fut/items/web/{0}.json";
+        public const string Item = "https://fifa15.content.easports.com/fifa/fltOnlineAssets/B488919F-23B5-497F-9FC0-CACFB38863D0/2016/fut/items/web/{0}.json";
 
-        public const string PlayerImage = "http://fifa15.content.easports.com/fifa/fltOnlineAssets/8D941B48-51BB-4B87-960A-06A61A62EBC0/2015/fut/items/images/players/web/{0}.png";
+        public const string PlayerImage = "http://fifa15.content.easports.com/fifa/fifa/fltOnlineAssets/B488919F-23B5-497F-9FC0-CACFB38863D0/2016/fut/items/images/players/web/{0}.png";
 
-        public const string FlagsImage = "http://fifa15.content.easports.com/fifa/fltOnlineAssets/8D941B48-51BB-4B87-960A-06A61A62EBC0/2015/fut/items/images/cardflagssmall/web/{0}.png";
+        public const string FlagsImage = "http://fifa15.content.easports.com/fifa/fltOnlineAssets/B488919F-23B5-497F-9FC0-CACFB38863D0/2016/fut/items/images/cardflagssmall/web/{0}.png";
 
-        public const string ClubImage = "http://fifa15.content.easports.com/fifa/fltOnlineAssets/8D941B48-51BB-4B87-960A-06A61A62EBC0/2015/fut/items/images/clubbadges/web/s{0}.png";
+        public const string ClubImage = "http://fifa15.content.easports.com/fifa/fltOnlineAssets/B488919F-23B5-497F-9FC0-CACFB38863D0/2016/fut/items/images/clubbadges/web/s{0}.png";
 
-        public string FutHome = "https://utas.s2.fut.ea.com/ut/game/fifa15/";
+        public string FutHome = "https://utas.s3.fut.ea.com/ut/game/fifa16/";
 
-        public const string FutHomeXbox360 = "https://utas.fut.ea.com/ut/game/fifa15/";
+        public const string FutHomeXbox360 = "https://utas.s3.fut.ea.com/ut/game/fifa16/";
 
         public const string MyClub = "club?level=10";
 
