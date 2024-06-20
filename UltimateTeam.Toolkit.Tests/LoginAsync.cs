b7d7a682d28e954ade07e255f59aa157c09d1668
tringler <thomas.ringler@googlemail.com>
2024-06-20 10:24:22 +0200

    Added basic tests

---
+++ UltimateTeam.Toolkit.Tests/LoginAsync.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UltimateTeam.Toolkit.Constants;
+using UltimateTeam.Toolkit.Factories;
+using UltimateTeam.Toolkit.Models;
+using UltimateTeam.Toolkit.Models.Auth;
+using UltimateTeam.Toolkit.Models.Generic;
+using UltimateTeam.Toolkit.Tests.Helpers;
+
+namespace UltimateTeam.Toolkit.Tests
+{
+    [TestClass]
+    public class LoginAsync : TestBase
+    {
+        [TestMethod]
+        public async Task LoginAsync_AssertSid()
+        {
+            LoginDetails loginDetails = new LoginDetails("<user>", "<password>", Platform.Ps5, AppVersion.WebApp);
+            LoginResponse loginResponse = await FutClient.LoginAsync(loginDetails, TwoFactorProvider);
+
+            Assert.IsNotNull(loginResponse.AuthData?.Sid);
+        }
+    }
+}
